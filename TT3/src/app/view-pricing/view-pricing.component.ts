18b466e6683635942c6caf97a4c7bbde21d1400f
yaoting96 <76780678+yaoting96@users.noreply.github.com>
2021-03-05 12:20:38 +0800

    Added with sample API response

--- TT3/src/app/view-pricing/view-pricing.component.ts
+++ TT3/src/app/view-pricing/view-pricing.component.ts
@@ -1,15 +1,41 @@
 import { Component, OnInit } from '@angular/core';
+import { getMatIconNameNotFoundError } from '@angular/material';
+import { TokenServiceService } from '../services/token-service.service';
+
+
 
 @Component({
   selector: 'app-view-pricing',
   templateUrl: './view-pricing.component.html',
   styleUrls: ['./view-pricing.component.css']
 })
+
+
+
+
 export class ViewPricingComponent implements OnInit {
 
-  constructor() { }
+currentPrice:any;
+
+
+  constructor()
+  {
+    this.currentPrice = 
+    {
+      "price": 32.28,
+      "assetSymbol": "TTK",
+      "timestamp": 1614916200
+    };
+  
+  };
+
 
   ngOnInit() {
-  }
 
+
+    
+    };
+    
+
+  
 }
