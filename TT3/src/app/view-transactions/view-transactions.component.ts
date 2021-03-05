7fd20423cddd45d70e5f3ab8f6cdbb8b54099b54
WeeSooJun <weesj96@gmail.com>
2021-03-05 15:25:58 +0800

    Added dependencies

--- TT3/src/app/view-transactions/view-transactions.component.ts
+++ TT3/src/app/view-transactions/view-transactions.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { getMatIconFailedToSanitizeLiteralError } from '@angular/material';
 import { TokenServiceService } from '../services/token-service.service';
 import { TransactionDetails } from '../services/types/transactionDetails';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-view-transactions',
