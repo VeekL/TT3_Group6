ed6a6b127ce8c10ea59be3c842642d6a87d031b5
VeekL <veeklee92@gmail.com>
2021-03-05 12:11:35 +0800

    Update token-service.service.ts

--- TT3/src/app/services/token-service.service.ts
+++ TT3/src/app/services/token-service.service.ts
@@ -15,8 +15,9 @@ import { BuyOrSellDetails } from './types/buyOrSellDetails';
   providedIn: 'root'
 })
 export class TokenServiceService {
-    
-  url='https://cors-anywhere.herokuapp.com/https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/';
+  headers = new HttpHeaders({'x-api-key':'QQCQyzK1Qr2DzI1CYF192334KzjSsOq19Bp7g7ZQ'})
+
+  url='https://849rs099m3.execute-api.ap-southeast-1.amazonaws.com/techtrek/';
 
   loginUrl=this.url+'/login'; //POST
   viewBalanceUrl= this.url+'/balance'; //POST
@@ -29,27 +30,27 @@ export class TokenServiceService {
   constructor(private http:HttpClient) { }
 
   userLogin(loginDetails:LoginDetails):Observable<LoginResponse>{
-    return this.http.post<LoginResponse>(this.loginUrl,loginDetails); //login details, username,password, 
+    return this.http.post<LoginResponse>(this.loginUrl,loginDetails,{headers:this.headers}); //login details, username,password, 
   }
 
   viewBalance(accountKey:ViewBalanceDetails):Observable<ViewBalanceResponse>{
-    return this.http.post<ViewBalanceResponse>(this.viewBalanceUrl,accountKey); //accountKey
+    return this.http.post<ViewBalanceResponse>(this.viewBalanceUrl,accountKey,{headers:this.headers}); //accountKey
   }
 
   viewPricing():Observable<ViewPricingResponse>{
-    return this.http.post<ViewPricingResponse>(this.viewPricingUrl,''); //Empty Body
+    return this.http.post<ViewPricingResponse>(this.viewPricingUrl,'',{headers:this.headers}); //Empty Body
   }
 
   viewHistoricalPricing():Observable<HistoricalPricingResponse>{
-    return this.http.post<HistoricalPricingResponse>(this.viewHistoryUrl,''); // Empty Body
+    return this.http.post<HistoricalPricingResponse>(this.viewHistoryUrl,'',{headers:this.headers}); // Empty Body
   }
 
   viewTransactions(accountKey:TransactionDetails):Observable<TransactionResponse>{
-    return this.http.post<TransactionResponse>(this.viewTransactionsUrl,accountKey); //accountKey
+    return this.http.post<TransactionResponse>(this.viewTransactionsUrl,accountKey,{headers:this.headers}); //accountKey
   }
 
   buyOrSell(transactionType:BuyOrSellDetails):Observable<any>{
-    return this.http.post<any>(this.buySellUrl,transactionType); // accountKey, orderType,assetAmount
+    return this.http.post<any>(this.buySellUrl,transactionType,{headers:this.headers}); // accountKey, orderType,assetAmount
   }
 
 }
