3cec9676c2e6a3c7e46807568fee3e329e60bcbf
Jr <chuajiaren@gmail.com>
2021-03-05 14:49:34 +0800

    Trying to draw graph

--- TT3/src/app/historical-pricing/historical-pricing.component.ts
+++ TT3/src/app/historical-pricing/historical-pricing.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenServiceService } from '../services/token-service.service';
+import Chart from 'chart.js';
 
 @Component({
   selector: 'app-historical-pricing',
@@ -12,6 +13,11 @@ export class HistoricalPricingComponent implements OnInit {
 
   data: any;
 
+  price = [];
+  assetsymbols = [];
+  timestamp = [];
+  linechart = [];
+
   constructor(private tokenService: TokenServiceService) { }
 
   ngOnInit() {
@@ -19,7 +25,33 @@ export class HistoricalPricingComponent implements OnInit {
       response => {
         this.data = response;
         console.log(this.data);
+        this.price = this.data.price;
+        this.assetsymbols = this.data.assetSymbol;
+        this.timestamp = this.data.timestamp;
       }
     );
+    this.lineChartMethod;
   }
+
+  // ngAfterViewInit() { this.lineChartMethod }
+
+  lineChartMethod() {
+    this.linechart = new Chart('linechart', {
+      type: 'line',
+      data: {
+        labels: ['jan', 'feb', 'mar', 'april', 'may', 'june'],
+        data: [1, 2, 3, 4, 5, 6]
+      },
+      options: {
+        scales: {
+          yAxes: [{
+            ticks: {
+              beginAtZero: true
+            }
+          }]
+        }
+      }
+    });
+  }
+
 }
