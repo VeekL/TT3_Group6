42caa0c47284529a11cd72f63ddcc737840ad4d4
VeekL <veeklee92@gmail.com>
2021-03-05 12:31:02 +0800

    Update

--- TT3/src/app/login/login.component.ts
+++ TT3/src/app/login/login.component.ts
@@ -1,11 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { TokenServiceService } from '../services/token-service.service';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { LoginDetails } from '../services/types/loginDetails';
+
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent implements OnInit {
+
+  loginForm:FormGroup;
+  loginDetails:LoginDetails;
+
 
   userLogin={
     "username":"Group6",
@@ -19,5 +26,17 @@ export class LoginComponent implements OnInit {
     })
   }
 
-  
+  get f(){
+    return this.loginForm.controls;
+  }
+
+  login(){
+    this.loginDetails={
+      "username":this.f.username.value,
+      "password":this.f.password.value
+    }
+    this.tokenService.userLogin(this.loginDetails).subscribe(response=>{
+      console.log(response);
+    })
+  }
 }
