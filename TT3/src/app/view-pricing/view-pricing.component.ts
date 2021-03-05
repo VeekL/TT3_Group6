0cfb75e21c67ccb5fff89dfb0f46c7f236b0bc2b
yaoting96 <76780678+yaoting96@users.noreply.github.com>
2021-03-05 12:51:36 +0800

    integrated API to call current info

--- TT3/src/app/view-pricing/view-pricing.component.ts
+++ TT3/src/app/view-pricing/view-pricing.component.ts
@@ -1,3 +1,4 @@
+import { ThrowStmt } from '@angular/compiler';
 import { Component, OnInit } from '@angular/core';
 import { getMatIconNameNotFoundError } from '@angular/material';
 import { TokenServiceService } from '../services/token-service.service';
@@ -17,28 +18,37 @@ export class ViewPricingComponent implements OnInit {
 
 currentPrice:any;
 currentDate:any;
+testPrice:any;
 
-  constructor()
+  constructor(private tokenService:TokenServiceService)
   {
-    this.currentPrice = 
-    {
-      "price": 32.28,
-      "assetSymbol": "TTK",
-      "timestamp": 1614916200
-    };
-
-    this.currentDate = Date(this.currentPrice.timestamp * 1000);
-    console.log(this.currentDate);
-  
   };
 
 
   ngOnInit() {
 
+    this.getInfo();
 
-    
+    // this.currentPrice = 
+    // {
+    //   "price": 32.28,
+    //   "assetSymbol": "TTK",
+    //   "timestamp": 1614916200
+    // };
+
+  
+
+
+
     };
     
+  getInfo()
+  {
+    this.tokenService.viewPricing().subscribe((response)=>{
+      this.currentPrice = response;
+      this.currentDate = Date(this.currentPrice.timestamp * 1000);
+    };
+  }
 
   
 }
